5cdfc53815afbd69c699f2a312176a2f54716561
Akhil <93485154+Akhil4264@users.noreply.github.com>
2024-07-08 01:41:40 +0530

    integrated tasks with frontend

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -11,10 +11,9 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 #[Route('/task')]
 class TaskController extends AbstractController
@@ -29,38 +28,84 @@ class TaskController extends AbstractController
     }
 
     #[Route('/', name: 'task_index', methods: ['GET'])]
-    public function index(Security $security): Response
+    public function index(): Response
     {
-        $repository=$this->entityManager->getRepository(Task::class);
-        $user = $security->getUser();
-        
+        $repository = $this->entityManager->getRepository(Task::class);
+        // $user = $security->getUser();
+
         $tasks = $repository->findAll();
         return $this->render('task/index.html.twig', [
             'tasks' => $tasks,
         ]);
     }
 
-    #[Route('/new', name: 'task_new', methods: ['GET', 'POST'])]
-    public function new(Request $request): Response
+    // #[Route('/new', name: 'task_new', methods: ['GET', 'POST'])]
+    // public function new(Request $request): Response
+    // {
+    //     $task = new Task();
+    //     $form = $this->createForm(TaskType::class, $task);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $task->setCreatedAt(new \DateTime());
+    //         $task->setUpdatedAt(new \DateTime());
+    //         $this->entityManager->persist($task);
+    //         $this->entityManager->flush();
+
+    //         return $this->redirectToRoute('task_index');
+    //     }
+
+    //     return $this->render('task/new.html.twig', [
+    //         'task' => $task,
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
+
+    #[Route('/new', name: 'task_new', methods: ['POST'])]
+    public function new(Request $request, UserRepository $userRepository): JsonResponse
     {
-        $task = new Task();
-        $form = $this->createForm(TaskType::class, $task);
-        $form->handleRequest($request);
+        $data = json_decode($request->getContent(), true);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $task->setCreatedAt(new \DateTime());
-            $task->setUpdatedAt(new \DateTime());
-            $this->entityManager->persist($task);
-            $this->entityManager->flush();
-
-            return $this->redirectToRoute('task_index');
+        if (!isset($data['name'], $data['description'], $data['priority'], $data['assigned_to'], $data['deadline'])) {
+            return new JsonResponse(['error' => 'Invalid data provided'], Response::HTTP_BAD_REQUEST);
         }
 
-        return $this->render('task/new.html.twig', [
-            'task' => $task,
-            'form' => $form->createView(),
-        ]);
+        $user = $userRepository->find($data['assigned_to']);
+
+        if (!$user) {
+            return new JsonResponse(['error' => 'Assigned user not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $task = new Task();
+        $task->setTitle($data['name']);
+        $task->setDescription($data['description']);
+        $task->setPriority($data['priority']);
+        $task->setAssignee($user);
+        $task->setPlannedDate(new \DateTime($data['deadline']));
+        $task->setCreatedAt(new \DateTime());
+        $task->setUpdatedAt(new \DateTime());
+
+        $this->entityManager->persist($task);
+        $this->entityManager->flush();
+
+        $taskDetails = [
+            'id' => $task->getId(),
+            'name' => $task->getTitle(),
+            'description' => $task->getDescription(),
+            'priority' => $task->getPriority(),
+            'assigned_to' => [
+                'id' => $user->getId(),
+                'name' => $user->getFullName(),
+                'email' => $user->getEmail(),
+            ],
+            'deadline' => $task->getPlannedDate()->format('Y-m-d H:i:s'),
+            'created_at' => $task->getCreatedAt()->format('Y-m-d H:i:s'),
+            'updated_at' => $task->getUpdatedAt()->format('Y-m-d H:i:s'),
+        ];
+
+        return new JsonResponse($taskDetails, Response::HTTP_CREATED);
     }
+
 
     #[Route('/{id}/edit', name: 'task_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Task $task): Response
@@ -83,7 +128,7 @@ class TaskController extends AbstractController
     #[Route('/{id}', name: 'task_show', methods: ['GET'])]
     public function show(int $id): Response
     {
-        $repository=$this->entityManager->getRepository(Task::class);
+        $repository = $this->entityManager->getRepository(Task::class);
         $task = $repository->find($id);
 
         if (!$task) {
@@ -101,7 +146,7 @@ class TaskController extends AbstractController
         $teamRepository = $this->entityManager->getRepository(Team::class);
         $team = $teamRepository->find($teamId);
         $userRepository = $this->entityManager->getRepository(User::class);
-        $taskrepository=$this->entityManager->getRepository(Task::class);
+        $taskrepository = $this->entityManager->getRepository(Task::class);
         if (!$team) {
             throw $this->createNotFoundException('The team does not exist');
         }
@@ -125,7 +170,7 @@ class TaskController extends AbstractController
     #[Route('/{id}', name: 'task_delete', methods: ['POST'])]
     public function delete(Request $request, Task $task): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$task->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $task->getId(), $request->request->get('_token'))) {
             $this->entityManager->remove($task);
             $this->entityManager->flush();
         }
@@ -138,7 +183,7 @@ class TaskController extends AbstractController
     {
         $userRepository = $this->entityManager->getRepository(User::class);
         $user = $userRepository->find($id);
-        $taskrepository=$this->entityManager->getRepository(Task::class);
+        $taskrepository = $this->entityManager->getRepository(Task::class);
 
         if (!$user) {
             throw $this->createNotFoundException('The user does not exist');
